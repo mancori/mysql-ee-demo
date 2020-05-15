764e75e423af32be8548da7f2d217137e15ba9e9
MySQL Server <mysql@audit-demo.sub03262026550.mysqlaasvcn.oraclevcn.com>
2020-05-15 23:42:57 +0000

    “fix”

--- meb-os-shell-scripts/01_meb_full.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export BCKUPDIR=/home/mysql/backup
-export BCKUPIMAGE=full_meb.img
-export LOGINPATH=cred_backup
-export SOCKET=/var/lib/mysql/mysql.sock
-echo "Executing hot backup MEB full..."                    
-mysqlbackup --login-path=$LOGINPATH --socket=$SOCKET --backup-dir=$BCKUPDIR --backup-image=$BCKUPIMAGE --compress --compress-level=1 --compress-method=lzma  backup-to-image
--- meb-os-shell-scripts/meb_full_restore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export BCKUPDIR=/data0/backups/2017-08-18_19-46-19
-export BCKUPIMAGE=full_meb.img
-export USER=root
-#export PASSWORD=sakila1995
-export DATADIR=/data0/CE/slave
-echo "Executing restore MEB full..."                    
-mysqlbackup  --user=$USER --password=$PASSWORD --backup-dir=$BCKUPDIR --backup-image=$BCKUPIMAGE --read-threads=2 --write-threads=2 --process-threads=8 --limit-memory=1280 --number-of-buffers=80 --uncompress --datadir=$DATADIR copy-back-and-apply-log
