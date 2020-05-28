f5cc13e13a70c2f26d61276f9f6ab1673b625555
mancori <manuel.contreras@oracle.com>
2020-05-28 09:55:19 -0500

    v1.2

--- enterprise-backup/01_meb_full.sh
+++ enterprise-backup/01_meb_full.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 export BCKUPDIR=/home/mysql/meb
 export BCKUPDIRTMP=/home/mysql/meb/tmp
-export BCKUPIMAGE=full_meb
+export BCKUPIMAGE=full_meb.img
 export USER=root
 export SOCKET=/var/lib/mysql/mysql.sock
 echo "Executing hot backup MEB(MySQL Enterprise Backup) full..."                    
 mysqlbackup -u$USER -p --socket=$SOCKET --backup-dir=$BCKUPDIRTMP --with-timestamp --backup-image=$BCKUPDIR/$BCKUPIMAGE.$(hostname).img --compress --compress-level=1 --compress-method=lzma  backup-to-image
-rm -rf $/home/mysql/meb/tmp
 echo "Backup Image on " $BCKUPDIR/$BCKUPIMAGE.$(hostname).img
